e8aa0c00eb36e87fdffb91db8af20688266ea2cb
Nguyen Dac Doan <nguyendacdoan@gmail.com>
2021-02-03 09:22:20 +0700

    chart

---
+++ chart/precache-manifest.2798ef1a658767787fabb9fb9a89e400.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ffbb9ea27f03668ab5c253aaa05be48",
+    "url": "./index.html"
+  },
+  {
+    "revision": "452f0725c1bc1b3bce86",
+    "url": "./static/css/main.4d9d0307.chunk.css"
+  },
+  {
+    "revision": "215b9d1ce9b00b3ee5b5",
+    "url": "./static/js/2.119ba884.chunk.js"
+  },
+  {
+    "revision": "728efa4d4469ada87b26880c61f0e3e5",
+    "url": "./static/js/2.119ba884.chunk.js.LICENSE"
+  },
+  {
+    "revision": "452f0725c1bc1b3bce86",
+    "url": "./static/js/main.d414662e.chunk.js"
+  },
+  {
+    "revision": "225154601ac88d602708",
+    "url": "./static/js/runtime-main.dbe67b53.js"
+  }
+]);
